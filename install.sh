ce6815e4ab39b2f4081c669290c6bd373dd89588
mainyaa <kazuyuki.mori@topgate.ne.jp>
2013-10-18 12:51:29 +0900

    Fixed backup old file if file exists

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@ mkdir -p ~/src
 git clone git@github.com:mainyaa/dotfiles.git ~/src/dotfiles
 cd ~/src/dotfiles
 
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc.old
+fi
+if [ -f ~/.bash_profile ]; then
+    mv ~/.bash_profile ~/.bash_profile.old
+fi
+if [ -f ~/.vim ]; then
+    mv ~/.vim ~/.vim.old
+fi
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.old
+fi
 ln -s ~/src/dotfiles/.bashrc ~/
 ln -s ~/src/dotfiles/.bash_profile ~/
 ln -s ~/src/dotfiles/.editorconfig ~/
@@ -18,7 +30,7 @@ mkdir -p ~/_vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/_vim/vundle.git
 
 ln -s ~/src/dotfiles/vimfiles/_vim ~/_vim
-ln -s ~/src/dotfiles/vimfiles/_gvimrc ~/_vimrc
+ln -s ~/src/dotfiles/vimfiles/_gvimrc ~/_gvimrc
 ln -s ~/src/dotfiles/vimfiles/_vimrc ~/_vimrc
 
 mkdir -p ~/_vim/dict
@@ -28,11 +40,11 @@ wget https://github.com/cooldaemon/myhome/blob/master/.vim/dict/javascript.dict
 mv javascript.dict ~/_vim/dict/
 
 vim +BundleInstall +qall
-npm install -g coffee
-npm install -g js2coffee
-npm install -g typescript
-npm install -g tslint
-npm install -g grunt
-npm install -g jslint
-npm install -g jshint
-npm install -g jstags
+sudo npm install -g coffee
+sudo npm install -g js2coffee
+sudo npm install -g typescript
+sudo npm install -g tslint
+sudo npm install -g grunt
+sudo npm install -g jslint
+sudo npm install -g jshint
+sudo npm install -g jstags
