357c03c2dcdf6dec19b1a88c158878af534e3b0b
mainyaa <kazuyuki.mori@topgate.ne.jp>
2014-01-14 12:19:24 +0900

    move to Brewfile

--- install_mac.sh
+++ install_mac.sh
@@ -1,75 +1,81 @@
-brew install tmux
-brew install reattach-to-user-namespace
-brew install tree
-brew install pidof
-brew install ctags
-brew install gdbm
-brew install maven
-brew install pcre
-brew install direnv
-brew install grunt
-brew install python
-brew install pythonbrew
-brew install spark
-brew install trash
-brew install yuicompressor
-brew install git
-brew install gibo
-brew install ag
-brew install node
-brew install phantomjs
-brew install readline
-brew install spidermonkey
-brew install z
-brew install ibevent
-brew install nspr
-brew install pkg-config
-brew install sbt
-brew install sqlite
-brew install watch
-brew install libtool
-brew install oniguruma
-brew install protobuf
-brew install scala
-brew install tig
-brew install wget
-brew install llvm
-brew install optipng
-brew install pstree
-brew install serf
-brew install zsh-completions
-brew install xz
-brew install direnv
-brew tap phinze/homebrew-cask
-brew install brew-cask
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install google-chrome
-brew cask install google-app-engine-launcher
-brew cask install google-drive
-brew cask install google-hangouts
-brew cask install growlnotify
-brew cask install gitx-l
-brew cask install github
-brew cask install bettertouchtool
-brew cask install gimp
-brew cask install mou
-brew cask install java
-brew cask install eclipse-ide
-brew cask install porthole
-brew cask install intellij-idea-community
-brew cask install android-studio
-brew cask install android-file-transfer
-brew cask install audacity
-brew cask install heroku-toolbelt
-brew cask install inkscape
-brew cask install jd-gui
-brew cask install synergy
-brew cask install keyremap4macbook
-brew cask install mi
-brew cask install skype
-brew cask install iterm2
-brew cask install libreoffice
-brew cask install kindle
-brew cask install keyfinder
-brew cask install sauce
+update
+
+upgrade
+
+install tmux
+install reattach-to-user-namespace
+install tree
+install pidof
+install ctags
+install gdbm
+install maven
+install pcre
+install direnv
+install grunt
+install python
+install pythonbrew
+install spark
+install trash
+install yuicompressor
+install git
+install gibo
+install ag
+install node
+install phantomjs
+install readline
+install spidermonkey
+install z
+install ibevent
+install nspr
+install pkg-config
+install sbt
+install sqlite
+install watch
+install libtool
+install oniguruma
+install protobuf
+install scala
+install tig
+install wget
+install llvm
+install optipng
+install pstree
+install serf
+install zsh-completions
+install xz
+install direnv
+tap phinze/homebrew-cask
+install brew-cask
+cask install virtualbox
+cask install vagrant
+cask install google-chrome
+cask install google-app-engine-launcher
+cask install google-drive
+cask install google-hangouts
+cask install growlnotify
+cask install gitx-l
+cask install github
+cask install bettertouchtool
+cask install gimp
+cask install mou
+cask install java
+cask install eclipse-ide
+cask install porthole
+cask install intellij-idea-community
+cask install android-studio
+cask install android-file-transfer
+cask install audacity
+cask install heroku-toolbelt
+cask install inkscape
+cask install jd-gui
+cask install synergy
+cask install keyremap4macbook
+cask install mi
+cask install skype
+cask install iterm2
+cask install libreoffice
+cask install kindle
+cask install keyfinder
+cask install sauce
+
+clean
